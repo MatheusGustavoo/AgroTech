11ae21f4139e57e05f8fd3712c5a0bffe4430923
Notebook_Matheus <matheussgustavoo@gmail.com>
2024-06-28 16:26:06 -0300

    Mudança do serviço de hospedagem de imagem

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 // import serviceAccount from "path/to/serviceAccountKey.json";
-const serviceAccount = process.env.PORT;
+const serviceAccount = process.env.SECRET;
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "apiagrotech.appspot.com",
