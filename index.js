02855e1646df055d6c2b2e95695c0bf7d04db461
Notebook_Matheus <matheussgustavoo@gmail.com>
2024-06-28 09:17:00 -0300

    Mudança da porta

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import rotasPost from "./routes/RotasPost.js";
 import rotasUsuario from "./routes/RotasUsuario.js";
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
