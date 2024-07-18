970507aeacdfb2ef8f3353385ca2fcb13611d258
Notebook_Matheus <matheussgustavoo@gmail.com>
2024-07-18 14:22:35 -0300

    Corrção do banco de dados

--- src/utils/Loading.jsx
+++ src/utils/Loading.jsx
@@ -1,12 +1,12 @@
 import Logo from "../../public/animado.svg?react";
 import styles from "../style/loading.module.css";
 
-export default function Loading(mensagem) {
+export default function Loading() {
   return (
     <div className={styles.loading}>
       <div className={styles.loadingAnimation}>
         <Logo />
-        {mensagem ? <h1>{mensagem}</h1> : <h1>Carregando as publicações!</h1>}
+        <h1>Carregando as publicações!</h1>
       </div>
     </div>
   );
